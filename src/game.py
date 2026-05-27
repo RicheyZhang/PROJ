475271c8a4c72af80775f79f457df0943cfd351e
Richey Zhang <richeyzhang06@gmail.com>
2026-05-27 04:06:06 -0400

    updated folders and gravy

--- src/game.py
+++ src/game.py
@@ -9,7 +9,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-font = pygame.font.Font('font/Pixeltype.ttf', 50)
+font = pygame.font.Font('../Assets/font/Pixeltype.ttf', 50)
 
 # fling animation (snail)
 snail_vel_x = 0
@@ -21,17 +21,21 @@ snail_hit = False
 # hit_surface_y = 0
 # hit_surface_x = False
 
+
 show_hit_timer = 0
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
+sky_surface = pygame.image.load('../Assets/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('../Assets/graphics/ground.png').convert()
 text_surface = font.render('', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail1.png').convert_alpha()
+snail_surface = pygame.image.load('../Assets/graphics/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomleft = (600, 300)) # builds snail hitbox
 
-player_surf = pygame.image.load('graphics/player_walk_1.png').convert_alpha()
+
+
+player_surf = pygame.image.load('../Assets/graphics/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300)) 
+player_gravity = 0
 
 hit_surface = font.render('HIT!', False, 'Red')
 
@@ -51,15 +55,31 @@ while True:
     if snail_rect.x < -100: snail_rect.x = 800
     screen.blit(snail_surface, snail_rect)
 
+    # Player
     if player_rect.right < 0:
         player_rect.left = 800
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+        player_gravity = 0 # reset gravity
     screen.blit(player_surf, player_rect)
+    print(player_rect.y)
+
+    # player gravity
+    # Character Jump
+    keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     player_vel_y = -10 # inital jump 
+
 
     #initial fling
     if player_rect.colliderect(snail_rect) and not snail_hit:
         snail_hit = True
-        snail_vel_x = 15 # flings right
-        snail_vel_y = -18 # flings upward
+        snail_vel_x = 10 # flings right
+        snail_vel_y = -13 # flings upward
         show_hit_timer = 30 # shows text for 60 frames
     if show_hit_timer > 0:
         screen.blit(hit_surface, (350, 50))
@@ -76,9 +96,9 @@ while True:
             snail_rect.x = 800
 
 
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print ("collision")
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print ("collision")
 
     
 
